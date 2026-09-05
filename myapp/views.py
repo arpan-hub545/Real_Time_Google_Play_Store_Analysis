6e4f5e4da9529850f8ee0dc18daa7f2b94720999
arpan-hub545 <arpandas022a@gmail.com>
2026-09-05 23:08:18 +0530

    Updated html

--- myapp/views.py
+++ myapp/views.py
@@ -2,12 +2,16 @@ from django.shortcuts import render
 
 from django.http import Http404
 
+
 from .analysis import plot_containers_11
 
 import myapp
 
 def dashboard(request):
     return render(request,"myapp/dashboard.html",{"plots": plot_containers_11})
+
+def project_report(request):
+    return render(request, "myapp/report.html")
 
 def graph_detail(request, graph_id):
     if graph_id < 0 or graph_id >= len(plot_containers_11):
--- projectgoogleanalysis/urls.py
+++ projectgoogleanalysis/urls.py
@@ -6,5 +6,6 @@ from myapp import views
 
 urlpatterns = [
     path("", views.dashboard, name="dashboard"),
+    path("project_report/",views.project_report,name="project_report"),
     path("graph/<int:graph_id>/", views.graph_detail, name="graph_detail")
 ]
